88e63b9cd8f47e12444df7f08c7bb4f8a2080572
teejaydub <tw@timothyweber.org>
2023-08-09 09:46:32 -0400

    Initial code commit.

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # saggydog
 Arduino firmware to monitor voltages and log their values to a serial port.
+
+## Building
+
+Should be widely compatible with hardware supporting Arduino.
+
+Set up for use with PlatformIO; includes a VSCode workspace.
+
+A build target for the ESP32-C3 DevKit M-1 board is included.  This mainly
+required some customizations to the ESP-IDF `sdkconfig`, to prevent logging
+other stuff to the serial port.
+
+## Configuration
+
+The key settings are currently in the source code, in `main.cpp`, in the `Feature configuration` section.
+
+It would be cool to replace those with a console interface via serial input,
+that would save the last-used settings to non-volatile storage.
+
+Defaults are currently to log the voltage on the Arduino pin `A1` every 4 seconds,
+and to go to "warning" mode below 3.0 V and to "error" mode below 2.7 V.
+
+## Usage
+
+Plug the programmed board into USB, or otherwise power it and connect it to a serial port.
+
+Monitor the serial output via a console program.
+
+## Tricks
+
+You can use my other project [Retrover](https://github.com/teejaydub/retrover)
+to monitor the serial output interleaved with the log from another board under development.
+
+I found this particularly useful for diagnosing problems caused by brownouts on a power supply.
+Retrover's output merges Saggydog's with the output of the board under test,
+so when a brownout caused the chip to reboot, the Retrover log looked like this:
+
+```
+2023-08-07 16:42:53.409611  > ~S=4 ?=1 ?*
+2023-08-07 16:42:53.741868 <  3.30 V
+2023-08-07 16:42:54.414825  > ~S=4 ?=1 ?*
+2023-08-07 16:42:54.798462 <  2.98 V ??
+2023-08-07 16:42:55.890083 <  2.79 V ??
+2023-08-07 16:42:57.106462  > CB 7.9.2
+2023-08-07 16:42:57.106462  > ~S=2 ?=1 ?P ?*
+2023-08-07 16:42:57.108566
+
+== EVENT FOUND ==
+
+2023-08-07 16:42:57.383441  > [P.]
+2023-08-07 16:42:58.101074  > ~S=2 ?=1 ?*
+2023-08-07 16:42:59.191474  > ~S=2 ?=1 ?*
+2023-08-07 16:42:59.797474 <  3.30 V
+```
+
+Retrover labels the Saggydog output with `< ` and the test board's output with ` >`.
+
+Normal output at nominal supply voltage (3.3 V) is then followed by dropping
+voltage, flagged with `??` and accompanied by a faster reporting rate,
+followed in turn by the message `CB 7.9.2` which is the version number of the
+board under test, output when the board reboots.  After rebooting, supply
+voltage returns to 3.30 V.
---
+++ src/main.cpp
@@ -0,0 +1,186 @@
+#include <Arduino.h>
+
+
+//===================================================================
+// Feature configuration
+
+#define LOG_PIN  A1  // An Arduino pin number or pin definition macro.
+#define LOG_BAUD  9600  // Baud rate to log on (may not matter for all serial ports on all chips).
+#define REPORT_PERIOD_S  4  // How often to report the voltage.
+
+// What to report for each reading:
+#define LOG_AD  1  // the raw A/D value
+
+#define LOG_VOLTAGE  2  // the voltage: requires setting SUPPLY_V and AD_MAX accurately.
+#define AD_MAX  4096  // the maximum value returned by the A/D on this chip.
+#define SUPPLY_V  3.3  // The reference for the A/D on this chip.
+
+#define LOG_MODE  LOG_VOLTAGE  // Choose one of the modes above.
+
+// #define LOG_COUNT  // Define to also note how many readings there were
+
+// Define these to flag when the minimum V gets to these voltages.
+// Currently requires LOG_MODE = LOG_VOLTAGE.
+#define WARNING_LEVEL_V  3.0
+#define ERROR_LEVEL_V  2.7
+
+// Define these to log more quickly when we've seen a warning or error state.
+// Can be floating-point expressions.
+#define WARNING_PERIOD_S  1
+#define ERROR_PERIOD_S 0.2
+
+
+//===================================================================
+// Internal use constants
+
+#define STATE_NORMAL  1
+#define STATE_WARNING  2
+#define STATE_ERROR  3
+int state = STATE_NORMAL;
+
+// Symbols for A/D conversion
+#define NO_READING  -1
+#define MS_PER_SEC  1000
+
+
+//===================================================================
+// Reading variables
+
+// The lowest reading we've seen recently, in D/A units.
+int minReading;
+unsigned long lastReport;
+unsigned long readingCount;
+
+
+void reset_reading(void)
+{
+  minReading = NO_READING;
+  lastReport = millis();
+  readingCount = 0;
+  state = STATE_NORMAL;
+}
+
+float reading_to_float(int reading)
+{
+  return reading * 1.0 / AD_MAX * SUPPLY_V;
+}
+
+void set_state(int newState)
+{
+  state = newState;
+}
+
+void accumulate_readings(void)
+{
+  int newValue = analogRead(LOG_PIN);
+  if (minReading == NO_READING)
+    minReading = newValue;
+  else
+    minReading = min(minReading, newValue);
+
+  #if defined(ERROR_LEVEL_V) || defined(WARNING_LEVEL_V)
+    float v = reading_to_float(newValue);
+  #endif
+
+  #ifdef ERROR_LEVEL_V 
+  if (state < STATE_ERROR && v <= ERROR_LEVEL_V)
+    set_state(STATE_ERROR);
+  #endif
+
+  #ifdef WARNING_LEVEL_V
+    #ifdef ERROR_LEVEL_V
+      else
+    #endif
+  if (state < STATE_WARNING && v <= WARNING_LEVEL_V)
+    set_state(STATE_WARNING);
+  #endif
+
+  #ifdef LOG_COUNT
+  ++readingCount;
+  #endif
+}
+
+void report(void)
+{
+  #if LOG_MODE == LOG_AD
+  Serial.printf("%d", minReading);
+  #elif LOG_MODE == LOG_VOLTAGE
+  Serial.printf("%.2f V", reading_to_float(minReading));
+  #endif
+
+  #ifdef LOG_COUNT
+  Serial.printf(" (%d)", readingCount);
+  #endif
+
+  #ifdef ERROR_LEVEL_V
+    if (state == STATE_ERROR)
+      Serial.print(" !!!!");
+  #endif
+
+  #ifdef WARNING_LEVEL_V
+    #ifdef ERROR_LEVEL_V
+      else
+    #endif
+    if (state == STATE_WARNING)
+      Serial.print(" ??");
+  #endif
+
+  Serial.println();
+}
+
+void maybe_report(void)
+{
+  int periodMs = REPORT_PERIOD_S * MS_PER_SEC;
+
+  #if defined(ERROR_LEVEL_V) && defined(ERROR_PERIOD_S)
+    if (state == STATE_ERROR)
+      periodMs = ERROR_PERIOD_S * MS_PER_SEC;
+  #endif
+
+  #if defined(WARNING_LEVEL_V) && defined(WARNING_PERIOD_S)
+    #if defined(ERROR_LEVEL_V) && defined(ERROR_PERIOD_S)
+      else
+    #endif
+    if (state == STATE_WARNING)
+      periodMs = WARNING_PERIOD_S * MS_PER_SEC;
+  #endif
+
+  if (millis() - lastReport > periodMs) {
+    report();
+    reset_reading();
+  }
+}
+
+void set_led_color(void)
+{
+  
+}
+
+void setup() {
+  pinMode(LOG_PIN, INPUT);
+  Serial.begin(LOG_BAUD);
+
+  Serial.println();
+  Serial.print("Saggydog: log min ");
+  #if LOG_MODE == LOG_AD
+    Serial.print("A/D value");
+  #elif LOG_MODE == LOG_VOLTAGE
+    Serial.print("voltage");
+  #endif
+  Serial.printf(" on pin %d.\n", LOG_PIN);
+
+  #ifdef ERROR_LEVEL_V
+  Serial.printf("Highlight errors below %.2f V.\n", ERROR_LEVEL_V);
+  #endif
+  #ifdef WARNING_LEVEL_V
+  Serial.printf("Warn below %.2f V.\n", WARNING_LEVEL_V);
+  #endif
+
+  reset_reading();
+}
+
+void loop() {
+  accumulate_readings();
+  set_led_color();
+  maybe_report();
+}
